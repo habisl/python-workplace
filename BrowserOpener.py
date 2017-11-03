1d1b8a54ff4de5c6ace5edf8cacf82db412a50e6
Habibul Islam <habibulislam72@yahoo.com>
2017-11-03 14:53:30 +0200

    Program for opening browser
    Program that can open multiple browser at a time in one clic

---
+++ BrowserOpener.py
@@ -0,0 +1,24 @@
+#The program can open multiple webpage at once
+#Author: Habibul
+
+import webbrowser
+import time
+
+socialMediaUrls=["https://twitter.com/", "www.facebook.com", "www.linkedin.com", ]
+techUrls= ["https://github.com/", "https: // www.udemy.com /"]
+
+
+def open_tabs(url_list):
+    for element in url_list:
+        webbrowser.open_new_tab(element)
+
+
+def main():
+    webbrowser.open("https://google.com",new=0, autoraise=False)
+    time.sleep(1)
+    open_tabs(socialMediaUrls)
+    open_tabs(techUrls)
+main()
+
+
+
