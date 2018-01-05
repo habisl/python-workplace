b2fc864911ed199de14e6e60109806ba7754fd79
Habibul Islam <habibulislam72@yahoo.com>
2018-01-05 11:49:04 +0200

    Update sshConnectionRemote.py

--- sshConnectionRemote.py
+++ sshConnectionRemote.py
@@ -5,7 +5,7 @@ ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 print("\nConnecting")
 
-ssh.connect('192.168.245.345', username='root', password='root')
+ssh.connect('192.168.245.345', username='xyz', password='xyz')
 print("\nConnected")
 
 cmd = "powershell -InputFormat none -OutputFormat TEXT Get-VMHost"
