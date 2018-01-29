0bde89b53038c5e6d131c630659296f084b6da80
Habibul Islam <habibulislam72@yahoo.com>
2018-01-29 03:36:44 +0200

    Update power.py

--- Udemy Quiz Solution/power.py
+++ Udemy Quiz Solution/power.py
@@ -5,3 +5,10 @@
 def power(n):
   return n**2
 print(power(2)) # Just to check if the power of 2 returns 4
+
+'''
+ Create a function and name it power2 . In difference to the previous power function this function should take two arguments, 
+ the base and the exponent  and should return the power of any base given any exponent.
+'''
+def power2(n,e):
+  return n**e
