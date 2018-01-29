81efb7e59bdd0f02b5d853d20af6cc8e2c220ba6
Habibul Islam <habibulislam72@yahoo.com>
2018-01-29 03:44:17 +0200

    Update power.py

--- Udemy Quiz Solution/power.py
+++ Udemy Quiz Solution/power.py
@@ -12,3 +12,13 @@ print(power(2)) # Just to check if the power of 2 returns 4
 '''
 def power2(n,e):
   return n**e
+
+ '''
+ Please add another line under the existing code where you use a print  function 
+ to print out the function call with values of 10  and 20  for parameters a  and b  respectively.
+ 
+ '''
+ def mysum(a, b):
+    return a + b
+print(mysum(10,20))
+ 
