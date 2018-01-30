ddf6add031b5f436e44436c0386de71c97429c89
Habibul Islam <habibulislam72@yahoo.com>
2018-01-30 17:05:19 +0200

    Create myfunction2.py

---
+++ Udemy Quiz Solution/myfunction2.py
@@ -0,0 +1,11 @@
+# Function with if and else statement
+
+def age_foo(age):
+  new_age = age + 50
+  return new_age
+age = float(input("Enter your age: "))
+
+if age < 150:
+ print (age_foo(age))
+else:
+print("How it is possible?")
