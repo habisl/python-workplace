7ef0d74ad26a5c9913a8e640f4891d4b129b4107
Habibul Islam <habibulislam72@yahoo.com>
2018-01-30 17:18:09 +0200

    Update quizes.py

--- Udemy Quiz Solution/quizes.py
+++ Udemy Quiz Solution/quizes.py
@@ -5,11 +5,13 @@ def age_foo(age):
 age = input("Enter your age: ")
 print(age_foo(age)) # Calculates the age that user will define
 
+
 # For Celsius to Farenheit. That would return 50.0 which means 10 degree Celsius is equal to 50 degree Fahrenheit.
 def cel_to_fahr(c):
     f = c * 9/5 + 32
     return f
 print(cel_to_fahr(10))
+
 
 '''
 Now, the lowest possible temperature that physical matter can reach is -273.15 °C. 
@@ -23,10 +25,13 @@ def c_to_f(c):
         f=c*9/5+32        
         return f
 print(c_to_f(-273.4))
+
+
 # A function that takes any string as argument and returns the length of that string.
 def string_length(mystring):
     return len(mystring)
 print(string_length("Hello"))
+
 
 #What would the following code output?
 
