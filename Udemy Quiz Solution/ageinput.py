dbc6663f06b02b47752c8417f2981475edfc52f4
Habibul Islam <habibulislam72@yahoo.com>
2018-01-30 16:33:53 +0200

    Update ageinput.py

--- Udemy Quiz Solution/ageinput.py
+++ Udemy Quiz Solution/ageinput.py
@@ -4,3 +4,9 @@ def age_foo(age):
 
 age = input("Enter your age: ")
 print(age_foo(age)) # Calculates the age that user will define
+
+# For Celsius to Farenheit. That would return 50.0 which means 10 degree Celsius is equal to 50 degree Fahrenheit.
+def cel_to_fahr(c):
+    f = c * 9/5 + 32
+    return f
+print(cel_to_fahr(10))
