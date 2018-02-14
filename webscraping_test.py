d968f88a3e35cfdb7c75119fdd59d90e0f47c4e5
Habibul Islam <22364354+habisl@users.noreply.github.com>
2018-02-14 23:52:58 +0200

    Create webscraping_test.py

---
+++ webscraping_test.py
@@ -0,0 +1,21 @@
+"""
+Sample example for knowing how to use BeutufulSoup library 
+for WebScraping
+"""
+
+
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("https://pythonhow.com/example.html")
+c = r.content
+
+soup = BeautifulSoup(c, "html.parser")
+all_cls = soup.find_all("div", {"class": "cities"})
+
+print(all_cls[0].find_all("h2")[0].text)
+
+for item in all_cls:
+    print(item.find_all("h2")[0].text)
+for item in all_cls:
+    print(item.find_all("p")[0].text)
