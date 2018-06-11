a299cf0c4f75c6052e139fb23a4ad4222e35b3cc
Habibul Islam <habibulislam72@yahoo.com>
2018-06-12 00:59:02 +0300

    converting list into set

---
+++ list_example.py
@@ -0,0 +1,6 @@
+# Efficient way of using list as a set
+
+restaurants = ["McDonald's", "Burger King", "McDonald's", "Chicken Chicken"]
+unique_restaurants = set(restaurants)
+print(unique_restaurants)
+# prints {'Chicken Chicken', "McDonald's", 'Burger King'}
