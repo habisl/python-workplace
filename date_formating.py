5fb882e2516bdb7a1f5abe3ae17ba6c629e74924
Habibul Islam <habibulislam72@yahoo.com>
2018-06-12 00:55:12 +0300

    Update date_formating.py

--- date_formating.py
+++ date_formating.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-datetime_for_string = datetime(2016,10,1,0,0)
+datetime_for_string = datetime(2018,10,1,0,0)
 datetime_string_format = '%b %d %Y, %H:%M:%S'
 datetime.strftime(datetime_for_string,datetime_string_format)
 print(datetime.strftime(datetime_for_string,datetime_string_format))
