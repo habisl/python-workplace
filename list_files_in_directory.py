ab93469ccde8a11c4be9ce2a762e67ef08af016f
Habibul Islam <habisl91@gmail.com>
2020-01-15 11:22:23 +0200

    Create list_files_in_directory.py

---
+++ list_files_in_directory.py
@@ -0,0 +1,8 @@
+import os
+
+# List all files in a directory using scandir()
+basepath = 'my_directory/'
+with os.scandir(basepath) as entries:
+    for entry in entries:
+        if entry.is_file():
+            print(entry.name)
